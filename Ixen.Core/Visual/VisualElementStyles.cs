4f007de2223109c8701a15218c97e841b26a717a
Kevin Piette <kevin@termina.org>
2022-04-24 16:29:53 +0200

    Added MarginStyle to handle margin and padding

--- Ixen.Core/Visual/VisualElementStyles.cs
+++ Ixen.Core/Visual/VisualElementStyles.cs
@@ -8,6 +8,8 @@ namespace Ixen.Core.Visual
         public LayoutStyle Layout { get; set; } = new();
         public SizeStyle Width { get; set; } = new();
         public SizeStyle Height { get; set; } = new();
+        public MarginStyle Margin { get; set; } = new();
+        public MarginStyle Padding { get; set; } = new();
 
         public BackgroundStyle Background { get; set; }
         public BorderStyle Border { get; set; }
