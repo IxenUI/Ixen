88fb7d52b31b909956c7fe30c32b389ea0811222
Kevin Piette <kevin@termina.org>
2023-04-09 17:53:24 +0200

    Updated visibility and packages

--- Ixen.Core/Assembly.cs
+++ Ixen.Core/Assembly.cs
@@ -1,10 +1,8 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Ixen.Core.UT")]
-[assembly: InternalsVisibleTo("Ixen.Language")]
+[assembly: InternalsVisibleTo("Ixen.Generators")]
 [assembly: InternalsVisibleTo("Ixen.Platform")]
 [assembly: InternalsVisibleTo("Ixen.Platform.Windows")]
 [assembly: InternalsVisibleTo("Ixen.View.WPF")]
 [assembly: InternalsVisibleTo("Ixen.View.Android")]
-
-[assembly: InternalsVisibleTo("Ixen.DemoApp")] 
