fc8d2c6c4ceb4d46c1cdf925ee6bdabd6344cf25
Kevin Piette <kevin@termina.org>
2023-04-16 23:48:47 +0200

    Updated tokenizer test

--- UnitTests/Ixen.Core.UT/Xns/XnsTokenizerTest.cs
+++ UnitTests/Ixen.Core.UT/Xns/XnsTokenizerTest.cs
@@ -37,15 +37,67 @@ namespace Ixen.Core.UT.Xns
     background: #FF2222
 }";
 
-            var xnsSource = XnsSource.FromSource(source);
+            var xnsSource = new XnsSource(source);
             var tokens = xnsSource.Tokenize();
 
             Assert.AreEqual(tokens.Count, 45);
 
-            Assert.AreEqual(tokens[0].LineNum, 0);
-            Assert.AreEqual(tokens[0].LineIndex, 0);
+            Assert.AreEqual(tokens[0].Index, 0);
             Assert.AreEqual(tokens[0].Content, "container");
             Assert.AreEqual(tokens[0].Type, XnsTokenType.ClassIdentifier);
+
+            Assert.AreEqual(tokens[1].Type, XnsTokenType.BeginClassContent);
+
+            Assert.AreEqual(tokens[2].Type, XnsTokenType.StyleName);
+            Assert.AreEqual(tokens[2].Content, "layout");
+            Assert.AreEqual(tokens[3].Type, XnsTokenType.StyleEquals);
+            Assert.AreEqual(tokens[4].Type, XnsTokenType.StyleValue);
+            Assert.AreEqual(tokens[4].Content, "row");
+
+            Assert.AreEqual(tokens[5].Type, XnsTokenType.StyleName);
+            Assert.AreEqual(tokens[5].Content, "width");
+            Assert.AreEqual(tokens[6].Type, XnsTokenType.StyleEquals);
+            Assert.AreEqual(tokens[7].Type, XnsTokenType.StyleValue);
+            Assert.AreEqual(tokens[7].Content, "100%");
+
+            Assert.AreEqual(tokens[8].Type, XnsTokenType.ClassIdentifier);
+            Assert.AreEqual(tokens[8].Content, "panel");
+
+            Assert.AreEqual(tokens[9].Type, XnsTokenType.BeginClassContent);
+
+            Assert.AreEqual(tokens[10].Type, XnsTokenType.StyleName);
+            Assert.AreEqual(tokens[10].Content, "width");
+            Assert.AreEqual(tokens[11].Type, XnsTokenType.StyleEquals);
+            Assert.AreEqual(tokens[12].Type, XnsTokenType.StyleValue);
+            Assert.AreEqual(tokens[12].Content, "50px");
+
+            Assert.AreEqual(tokens[13].Type, XnsTokenType.StyleName);
+            Assert.AreEqual(tokens[13].Content, "background");
+            Assert.AreEqual(tokens[14].Type, XnsTokenType.StyleEquals);
+            Assert.AreEqual(tokens[15].Type, XnsTokenType.StyleValue);
+            Assert.AreEqual(tokens[15].Content, "#222222");
+
+            Assert.AreEqual(tokens[16].Type, XnsTokenType.EndClassContent);
+
+            Assert.AreEqual(tokens[29].Type, XnsTokenType.ClassIdentifier);
+            Assert.AreEqual(tokens[29].Content, "entries");
+
+            Assert.AreEqual(tokens[30].Type, XnsTokenType.BeginClassContent);
+
+            Assert.AreEqual(tokens[31].Type, XnsTokenType.StyleName);
+            Assert.AreEqual(tokens[31].Content, "layout");
+            Assert.AreEqual(tokens[32].Type, XnsTokenType.StyleEquals);
+            Assert.AreEqual(tokens[33].Type, XnsTokenType.StyleValue);
+            Assert.AreEqual(tokens[33].Content, "column");
+
+            Assert.AreEqual(tokens[34].Type, XnsTokenType.ClassIdentifier);
+            Assert.AreEqual(tokens[34].Content, "entry");
+
+            Assert.AreEqual(tokens[35].Type, XnsTokenType.BeginClassContent);
+            Assert.AreEqual(tokens[36].Type, XnsTokenType.EndClassContent);
+
+            Assert.AreEqual(tokens[37].Type, XnsTokenType.EndClassContent);
+            Assert.AreEqual(tokens[38].Type, XnsTokenType.EndClassContent);
         }
     }
 }
