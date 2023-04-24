33c7c23d8c5b96c94c82f32b2dc03f54a41bfd1f
Kevin Piette <kevin@termina.org>
2023-04-24 21:16:24 +0200

    Styles handlers must be non null too

--- Ixen.Core/Visual/VisualElementStylesHandlers.cs
+++ Ixen.Core/Visual/VisualElementStylesHandlers.cs
@@ -4,15 +4,15 @@ namespace Ixen.Core.Visual
 {
     internal class VisualElementStylesHandlers
     {
-        public BackgroundStyleHandler Background { get; set; }
-        public BorderStyleHandler Border { get; set; }
-        public ColumnTemplateStyleHandler ColumnTemplate { get; set; }
-        public HeightStyleHandler Height { get; set; }
-        public LayoutStyleHandler Layout { get; set; }
-        public MarginStyleHandler Margin { get; set; }
-        public MaskStyleHandler Mask { get; set; }
-        public PaddingStyleHandler Padding { get; set; }
-        public RowTemplateStyleHandler RowTemplate { get; set; }
-        public WidthStyleHandler Width { get; set; }
+        public BackgroundStyleHandler Background { get; set; } = new();
+        public BorderStyleHandler Border { get; set; } = new();
+        public ColumnTemplateStyleHandler ColumnTemplate { get; set; } = new();
+        public HeightStyleHandler Height { get; set; } = new();
+        public LayoutStyleHandler Layout { get; set; } = new();
+        public MarginStyleHandler Margin { get; set; } = new();
+        public MaskStyleHandler Mask { get; set; } = new();
+        public PaddingStyleHandler Padding { get; set; } = new();
+        public RowTemplateStyleHandler RowTemplate { get; set; } = new();
+        public WidthStyleHandler Width { get; set; } = new();
     }
 }
