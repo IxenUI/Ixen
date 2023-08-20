152d996e675b4592d6b8c2b9563978f6872b7128
Kevin Piette <kevin@termina.org>
2023-08-20 19:25:24 +0200

    Added padding layout xnl/xns tests

--- UnitTests/Ixen.Core.UT/Layout/Padding/PaddingLayoutTests.cs
+++ UnitTests/Ixen.Core.UT/Layout/Padding/PaddingLayoutTests.cs
@@ -1,9 +1,8 @@
 ﻿using Ixen.Core.Visual;
-using Ixen.Core.Visual.Styles;
 using Ixen.Core.Visual.Styles.Descriptors;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Ixen.Core.UT.Layout
+namespace Ixen.Core.UT.Layout.Padding
 {
     [TestClass]
     public class PaddingLayoutTests : BaseVisualTests
@@ -13,7 +12,7 @@ namespace Ixen.Core.UT.Layout
         {
             var root = new VisualElement();
             root.Styles.Layout = new LayoutStyleDescriptor { Type = LayoutType.Row };
-            root.Styles.Background = new BackgroundStyleDescriptor { Color = Color.WhiteSmoke.ToRGBHexColor() };
+            root.Styles.Background = new BackgroundStyleDescriptor { Color = "#F5F5F5" };
 
             var sd10 = new SizeStyleDescriptor { Unit = SizeUnit.Pixels, Value = 10 };
             var sd50 = new SizeStyleDescriptor { Unit = SizeUnit.Pixels, Value = 50 };
@@ -23,17 +22,17 @@ namespace Ixen.Core.UT.Layout
             el1.Styles.Width = new WidthStyleDescriptor { Unit = SizeUnit.Pixels, Value = 200 };
             el1.Styles.Height = new HeightStyleDescriptor { Unit = SizeUnit.Pixels, Value = 100 };
             el1.Styles.Padding = new PaddingStyleDescriptor { Top = sd300, Right = sd300, Bottom = sd300, Left = sd300 };
-            el1.Styles.Background = new BackgroundStyleDescriptor { Color = Color.Red.ToRGBHexColor() };
+            el1.Styles.Background = new BackgroundStyleDescriptor { Color = "#FF0000" };
 
             var el2 = new VisualElement();
             el2.Styles.Width = new WidthStyleDescriptor { Unit = SizeUnit.Weight, Value = 1 };
             el2.Styles.Height = new HeightStyleDescriptor { Unit = SizeUnit.Pixels, Value = 300 };
-            el2.Styles.Margin = new MarginStyleDescriptor { Top = sd10, Right = sd50, Bottom = sd10, Left = sd50 };
-            el2.Styles.Background = new BackgroundStyleDescriptor { Color = Color.Blue.ToRGBHexColor() };
+            el2.Styles.Padding = new PaddingStyleDescriptor { Top = sd10, Right = sd50, Bottom = sd10, Left = sd50 };
+            el2.Styles.Background = new BackgroundStyleDescriptor { Color = "#0000FF" };
 
             root.AddChildren(el1, el2);
 
-            AssertVisual("393790e901c0fa8c253c9f601e5c3fb5", root);
+            AssertVisual("aead6cc112183963c9ae827519babe28", root);
         }
     }
 }
---
+++ UnitTests/Ixen.Core.UT/Layout/Padding/PaddingLayoutXnlTests.cs
@@ -0,0 +1,14 @@
+﻿using Ixen.Core.Components;
+using Ixen.Views;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ixen.Core.UT.Layout.Padding
+{
+    [TestClass]
+    public class PaddingLayoutXnlTests : BaseVisualTests
+    {
+        [TestMethod]
+        public void TestPaddingLayout1() 
+            => AssertVisual("aead6cc112183963c9ae827519babe28", new Component<PaddingLayoutTest1View>().View);
+    }
+}
